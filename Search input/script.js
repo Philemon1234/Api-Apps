524cdec68bb749cb93b292df2b0fd97f82404ba6
Philemon1234 <ntivuguruzwaphilemon0@gmai.com>
2023-03-05 14:57:05 +0300

    Added files

--- Search input/script.js
+++ Search input/script.js
@@ -4,7 +4,12 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then(res => res.json())
 .then(data => {
     data.forEach(user => {
-        // const card = usercardTemplate.content.cloneNode(true).children[0]
-        console.log(user); 
+        const card = userCardTemplate.content.cloneNode(true).children[0]
+        const header = card.document.querySelector("[data-header]");
+        const body = card.document.querySelector("[data-body]");
+        header.textContent = user.name;
+        body.textContent = user.email;
+        userCardContainer.append(card);
+        // console.log(user); 
     });
 })
